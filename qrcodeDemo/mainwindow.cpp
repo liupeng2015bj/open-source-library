ad6d42e687890310a8adeef99aef1939708b2bbf
liupeng2015bj <liupeng2015bj@163.com>
2021-02-08 13:18:56 +0800

    添加qrencode生成二维码图片Demo

---
+++ qrcodeDemo/mainwindow.cpp
@@ -0,0 +1,59 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+extern "C"{
+#include "qrencode.h"
+}
+#include <QImage>
+#include <QPainter>
+#include <QColor>
+#include <QPixmap>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+//    QRcode *qrcode = QRcode_encodeString("Hello World!", 2, QR_ECLEVEL_Q, QR_MODE_8, 0);
+    GenerateQRcode("Hello World!");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::GenerateQRcode(QString tempstr)
+{
+    QRcode *qrcode; //二维码数据
+    //QR_ECLEVEL_Q 容错等级
+    qrcode = QRcode_encodeString(tempstr.toStdString().c_str(), 2, QR_ECLEVEL_Q, QR_MODE_8, 1);
+    qint32 temp_width=ui->label->width(); //二维码图片的大小
+    qint32 temp_height=ui->label->height();
+    qint32 qrcode_width = qrcode->width > 0 ? qrcode->width : 1;
+    double scale_x = (double)temp_width / (double)qrcode_width; //二维码图片的缩放比例
+    double scale_y =(double) temp_height /(double) qrcode_width;
+    QImage mainimg=QImage(temp_width,temp_height,QImage::Format_ARGB32);
+    QPainter painter(&mainimg);
+    QColor background(Qt::white);
+    painter.setBrush(background);
+    painter.setPen(Qt::NoPen);
+    painter.drawRect(0, 0, temp_width, temp_height);
+    QColor foreground(Qt::black);
+    painter.setBrush(foreground);
+    for( qint32 y = 0; y < qrcode_width; y ++)
+    {
+        for(qint32 x = 0; x < qrcode_width; x++)
+        {
+            unsigned char b = qrcode->data[y * qrcode_width + x];
+            if(b & 0x01)
+            {
+                QRectF r(x * scale_x, y * scale_y, scale_x, scale_y);
+                painter.drawRects(&r, 1);
+            }
+        }
+    }
+    QPixmap mainmap=QPixmap::fromImage(mainimg);
+    ui->label->setPixmap(mainmap);
+    ui->label->setVisible(true);
+}
